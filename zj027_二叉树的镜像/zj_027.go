b1a65614eddb3311cb4c8b8f193f3b16af2d499d
jeffreyhan <jeffreyhan@tencent.com>
2022-06-27 10:38:00 +0800

    add

---
+++ zj027_二叉树的镜像/zj_027.go
@@ -0,0 +1,21 @@
+package zj027_二叉树的镜像
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Left == nil && root.Right == nil {
+		return root
+	}
+	root.Left, root.Right = root.Right, root.Left
+	mirrorTree(root.Left)
+	mirrorTree(root.Right)
+	return root
+}
